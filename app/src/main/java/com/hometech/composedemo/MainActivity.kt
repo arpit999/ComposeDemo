07424162334c65eeace903577dbc915d403e2168
Arpit Patel <arpit@temi.ca>
2022-05-30 00:05:38 -0400

    Button onclick with remember state on variable, Display list of data in LazyColumn with ust few lines of code. Whoohoooo.....

--- app/src/main/java/com/hometech/composedemo/MainActivity.kt
+++ app/src/main/java/com/hometech/composedemo/MainActivity.kt
@@ -6,17 +6,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hometech.composedemo.ui.theme.ComposeDemoTheme
@@ -37,6 +44,11 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun CreateBizCard() {
+
+        val buttonCLickState = remember {
+            mutableStateOf(false)
+        }
+
         Surface(
             modifier = Modifier
                 .fillMaxWidth()
@@ -67,8 +79,16 @@ class MainActivity : ComponentActivity() {
 
                     Button(
                         modifier = Modifier.padding(3.dp),
-                        onClick = { Log.d("Clicked", "CreateBizCard: Clicked") }) {
+                        onClick = {
+                            buttonCLickState.value = !buttonCLickState.value
+                        }) {
                         Text(text = "Portfolio", style = MaterialTheme.typography.button)
+                    }
+
+                    if (buttonCLickState.value) {
+                        Content()
+                    } else {
+                        Box {}
                     }
                 }
 
@@ -98,9 +118,9 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun CreateImageProfile() {
+    private fun CreateImageProfile(modifier: Modifier = Modifier) {
         Surface(
-            modifier = Modifier
+            modifier = modifier
                 .size(150.dp)
                 .padding(5.dp),
             shape = CircleShape,
@@ -112,15 +132,88 @@ class MainActivity : ComponentActivity() {
             Image(
                 painter = painterResource(id = R.drawable.profile_picture),
                 contentDescription = "Profile picture",
-                modifier = Modifier.size(135.dp),
+                modifier = modifier.size(135.dp),
                 contentScale = ContentScale.Crop
             )
 
         }
     }
 
-
     @Preview(showBackground = true)
+    @Composable
+    fun Content() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(5.dp)
+        ) {
+
+            Surface(
+                modifier = Modifier
+                    .padding(3.dp)
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(6.dp),
+                border = BorderStroke(width = 2.dp, color = Color.LightGray)
+            ) {
+
+                Portfolio(
+                    data = listOf(
+                        "Project 1",
+                        "Project 2",
+                        "Project 3",
+                        "Project 1",
+                        "Project 2",
+                        "Project 3",
+                        "Project 1",
+                        "Project 2",
+                        "Project 3"
+                    )
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Portfolio(data: List<String>) {
+        LazyColumn {
+            items(data) { item ->
+
+                Card(
+                    modifier = Modifier
+                        .padding(13.dp)
+                        .fillMaxWidth(), shape = RectangleShape,
+                    elevation = 4.dp
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .background(MaterialTheme.colors.surface)
+                            .padding(7.dp)
+                    ) {
+                        CreateImageProfile(modifier = Modifier.size(60.dp))
+
+                        Column(
+                            modifier = Modifier
+                                .padding(7.dp)
+                                .align(alignment = Alignment.CenterVertically)
+                        ) {
+                            Text(item, fontWeight = FontWeight.Bold)
+                            Text("A great Project", style = MaterialTheme.typography.body2)
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+    }
+
+
+    //    @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         ComposeDemoTheme {
