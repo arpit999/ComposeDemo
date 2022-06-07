0499a9d054f2df62b3bcbe4afb021420d6470bdf
Arpit Patel <arpit@temi.ca>
2022-06-07 16:34:02 -0400

    Intro App: Circle button and click listener.

--- app/src/main/java/com/hometech/composedemo/MainActivity.kt
+++ app/src/main/java/com/hometech/composedemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -27,196 +28,64 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hometech.composedemo.ui.theme.ComposeDemoTheme
+import org.intellij.lang.annotations.PrintFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDemoTheme {
-
-                Surface(color = MaterialTheme.colors.background) {
-                    CreateBizCard()
-                }
+                MyApp()
             }
         }
     }
 
-
     @Composable
-    fun CreateBizCard() {
-
-        val buttonCLickState = remember {
-            mutableStateOf(false)
-        }
-
-        Surface(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()) {
-            Card(
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(390.dp)
-                    .padding(12.dp),
-                shape = RoundedCornerShape(corner = CornerSize(15.dp)), elevation = 4.dp
-            ) {
-
-                Column(
-                    modifier = Modifier.height(300.dp),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-
-                    CreateImageProfile()
-                    Divider(
-                        modifier = Modifier.padding(0.dp, 5.dp),
-                        Color.LightGray
-                    )
-
-                    CreateInfo()
-
-                    Button(
-                        modifier = Modifier.padding(3.dp),
-                        onClick = {
-                            buttonCLickState.value = !buttonCLickState.value
-                        }) {
-                        Text(text = "Portfolio", style = MaterialTheme.typography.button)
-                    }
-
-                    if (buttonCLickState.value) {
-                        Content()
-                    } else {
-                        Box {}
-                    }
-                }
-
-            }
-
-        }
-    }
-
-    @Composable
-    private fun CreateInfo() {
-        Column(
-            modifier = Modifier.padding(5.dp)
-        ) {
-            Text(
-                text = "Miles P.",
-                style = MaterialTheme.typography.h4,
-                color = MaterialTheme.colors.primaryVariant
-            )
-            Text(text = "Android Compose Programmer", modifier = Modifier.padding(3.dp))
-            Text(
-                text = "@theCompose",
-                modifier = Modifier.padding(3.dp),
-                style = MaterialTheme.typography.subtitle2
-            )
-
-        }
-    }
-
-    @Composable
-    private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    fun MyApp() {
         Surface(
-            modifier = modifier
-                .size(150.dp)
-                .padding(5.dp),
-            shape = CircleShape,
-            border = BorderStroke(.5.dp, Color.LightGray),
-            elevation = 4.dp,
-            color = MaterialTheme.colors.surface.copy(alpha = .5f)
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            color = Color(0xFF546E7A)
         ) {
 
-            Image(
-                painter = painterResource(id = R.drawable.profile_picture),
-                contentDescription = "Profile picture",
-                modifier = modifier.size(135.dp),
-                contentScale = ContentScale.Crop
-            )
+            Box(contentAlignment = Alignment.Center) {
+                CreateCircle()
+            }
 
+        }
+    }
+
+
+    @Preview
+    @Composable
+    fun CreateCircle() {
+        Card(
+            modifier = Modifier
+                .padding(3.dp)
+                .size(90.dp),
+            shape = CircleShape,
+            elevation = 8.dp
+        ) {
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.clickable {
+                    Log.d("MainActivity", "CreateCircle: click")
+                }) {
+
+                Text(text = "TAP")
+            }
         }
     }
 
     @Preview(showBackground = true)
     @Composable
-    fun Content() {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(5.dp)
-        ) {
-
-            Surface(
-                modifier = Modifier
-                    .padding(3.dp)
-                    .fillMaxHeight()
-                    .fillMaxWidth(),
-                shape = RoundedCornerShape(6.dp),
-                border = BorderStroke(width = 2.dp, color = Color.LightGray)
-            ) {
-
-                Portfolio(
-                    data = listOf(
-                        "Project 1",
-                        "Project 2",
-                        "Project 3",
-                        "Project 1",
-                        "Project 2",
-                        "Project 3",
-                        "Project 1",
-                        "Project 2",
-                        "Project 3"
-                    )
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun Portfolio(data: List<String>) {
-        LazyColumn {
-            items(data) { item ->
-
-                Card(
-                    modifier = Modifier
-                        .padding(13.dp)
-                        .fillMaxWidth(), shape = RectangleShape,
-                    elevation = 4.dp
-                ) {
-
-                    Row(
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .background(MaterialTheme.colors.surface)
-                            .padding(7.dp)
-                    ) {
-                        CreateImageProfile(modifier = Modifier.size(60.dp))
-
-                        Column(
-                            modifier = Modifier
-                                .padding(7.dp)
-                                .align(alignment = Alignment.CenterVertically)
-                        ) {
-                            Text(item, fontWeight = FontWeight.Bold)
-                            Text("A great Project", style = MaterialTheme.typography.body2)
-                        }
-
-                    }
-
-                }
-
-            }
-        }
-    }
-
-
-    //    @Preview(showBackground = true)
-    @Composable
     fun DefaultPreview() {
-        ComposeDemoTheme {
-            CreateBizCard()
+        ComposeDemoTheme() {
+            MyApp()
         }
     }
+
 
 }
-
