79803cfe7451376377dc4812d6ecdf96de1a6d0c
Arpit Patel <arpit@temi.ca>
2022-06-22 16:08:12 -0400

    TipCalculator App: - Add Composable params in MyApp - Create TopContainer with rounded corner - Start Creating main content and Create separate file for Components.kt for TextField and pass argument in composable function so we can later use it for other textField with customize option. NOTE: Added keyboard hide code but does not work properly. Need some research or help.

--- app/src/main/java/com/hometech/composedemo/MainActivity.kt
+++ app/src/main/java/com/hometech/composedemo/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.hometech.composedemo
 
 import android.os.Bundle
@@ -7,17 +9,22 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hometech.composedemo.ui.InputField
 import com.hometech.composedemo.ui.theme.ComposeDemoTheme
 import com.hometech.composedemo.ui.theme.Purple100
 import com.hometech.composedemo.ui.theme.Purple200
@@ -27,7 +34,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+//                TopHeader()
+                MainContent()
             }
         }
     }
@@ -77,13 +85,37 @@ class MainActivity : ComponentActivity() {
 
     @Preview
     @Composable
-    fun MainContent() {
+    private fun MainContent() {
+        val totalBillState = remember {
+            mutableStateOf("")
+        }
+
+        val validState = remember(totalBillState) {
+            totalBillState.value.trim().isNotEmpty()
+        }
+
+        val keyBoardController = LocalSoftwareKeyboardController.current
+
         Surface(
             modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(corner = CornerSize(8.dp)),
             border = BorderStroke(width = 2.dp, color = Color.LightGray)
         ) {
             Column() {
+                InputField(
+                    valueState = totalBillState,
+                    labelId = "Enter Bill",
+                    enabled = true,
+                    isSingleLine = true,
+                    onAction = KeyboardActions {
+
+                        if (!validState) return@KeyboardActions
+                        //Todo -> OnValueChanged
+
+                        keyBoardController?.hide()
+                    }
+                )
+
 
             }
         }
--- app/src/main/java/com/hometech/composedemo/ui/Components.kt
+++ app/src/main/java/com/hometech/composedemo/ui/Components.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material.icons.rounded.AttachMoney
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
@@ -20,13 +21,13 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun InputField(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     valueState: MutableState<String>,
     labelId: String,
     enabled: Boolean,
     isSingleLine: Boolean,
     keyboardType: KeyboardType = KeyboardType.Number,
-    imeAction: ImeAction = ImeAction.Next,
+    imeAction: ImeAction = ImeAction.Done,
     onAction: KeyboardActions = KeyboardActions.Default
 ) {
     OutlinedTextField(
@@ -38,7 +39,7 @@ fun InputField(
         textStyle = TextStyle(fontSize = 12.sp, color = MaterialTheme.colors.onBackground),
         leadingIcon = {
             Icon(
-                imageVector = Icons.Rounded.AccountBox,
+                imageVector = Icons.Rounded.AttachMoney,
                 contentDescription = "Money Icon"
             )
         },
