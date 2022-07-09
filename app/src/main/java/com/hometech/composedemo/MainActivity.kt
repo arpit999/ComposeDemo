a2277cbd13f61db3caed4cb9656891189ca67da2
Arpit Patel <arpit@temi.ca>
2022-07-09 18:45:19 -0400

    Bottom Navigation Bar with Badge

--- app/src/main/java/com/hometech/composedemo/MainActivity.kt
+++ app/src/main/java/com/hometech/composedemo/MainActivity.kt
@@ -1,140 +1,196 @@
 package com.hometech.composedemo
 
+import android.accessibilityservice.AccessibilityButtonController
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.hometech.composedemo.ui.InputField
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.hometech.composedemo.ui.theme.ComposeDemoTheme
-import com.hometech.composedemo.ui.theme.Purple100
 
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp {
-//                TopHeader()
-                MainContent()
-            }
-        }
-    }
-
-    @Composable
-    fun MyApp(content: @Composable () -> Unit) {
-        ComposeDemoTheme {
-            Surface {
-                content()
-            }
-        }
-    }
-
-
-    //    @Preview
-    @Composable
-    fun TopHeader(totalPerPerson: Double = 0.00) {
-
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .clip(shape = RoundedCornerShape(12.dp)),
-            color = Purple100
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                val total = "%.2f".format(totalPerPerson)
-
-                Text(
-                    style = MaterialTheme.typography.h5,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    text = "Total Per Person"
-                )
-
-                Text(fontWeight = FontWeight.ExtraBold, fontSize = 40.sp, text = "$$total")
-
-            }
-
-        }
-
-    }
-
-    @ExperimentalComposeUiApi
-    @Preview
-    @Composable
-    private fun MainContent() {
-
-        BillForm() { billAmount ->
-            Log.d("Amount", "BillAmount: $billAmount")
-
-        }
-
-    }
-
-    @ExperimentalComposeUiApi
-    @Composable
-    fun BillForm(modifier: Modifier = Modifier, onValChange: (String) -> Unit = {}) {
-
-        val totalBillState = remember {
-            mutableStateOf("")
-        }
-
-        val validState = remember(totalBillState) {
-            totalBillState.value.trim().isNotEmpty()
-        }
-
-        val keyBoardController = LocalSoftwareKeyboardController.current
-
-        Surface(
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-            border = BorderStroke(width = 2.dp, color = Color.LightGray)
-        ) {
-            Column() {
-                InputField(
-                    valueState = totalBillState,
-                    labelId = "Enter Bill",
-                    enabled = true,
-                    isSingleLine = true,
-                    onAction = KeyboardActions {
-
-                        if (!validState) return@KeyboardActions
-
-                        onValChange(totalBillState.value.trim())
-
-                        keyBoardController?.hide()
+            ComposeDemoTheme {
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            item = listOf(
+                                BottomNavigationItem(
+                                    name = "Home",
+                                    route = "home",
+                                    icon = Icons.Default.Home
+                                ),
+                                BottomNavigationItem(
+                                    name = "Chat",
+                                    route = "chat",
+                                    icon = Icons.Default.Notifications,
+                                    badgeCount = 23
+                                ),
+                                BottomNavigationItem(
+                                    name = "Settings",
+                                    route = "settings",
+                                    icon = Icons.Default.Settings,
+                                    badgeCount = 214
+                                ),
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
                     }
-                )
+                ){
+                    Navigation(navController = navController)
+                }
+            }
+        }
+    }
+}
+
+    @Composable
+    fun Navigation(navController: NavHostController) {
+
+        NavHost(navController = navController, startDestination = "home") {
+
+            composable("home") {
+                HomeScreen()
+            }
+
+            composable("chat") {
+                ChatScreen()
+            }
+
+            composable("settings") {
+                SettingScreen()
+            }
+        }
+
+    }
+
+
+    @Composable
+    fun BottomNavigationBar(
+        item: List<BottomNavigationItem>,
+        navController: NavController,
+        modifier: Modifier = Modifier,
+        onItemClick: (BottomNavigationItem) -> Unit
+    ) {
+        val backStackEntry = navController.currentBackStackEntryAsState()
+        BottomNavigation(
+            modifier = modifier,
+            backgroundColor = Color.DarkGray,
+            elevation = 5.dp
+        ) {
+
+            item.forEach { item ->
+                val selected = item.route == backStackEntry.value?.destination?.route
+
+                BottomNavigationItem(
+                    selected = selected,
+                    onClick = { onItemClick(item) },
+                    selectedContentColor = Color.Green,
+                    unselectedContentColor = Color.DarkGray,
+                    icon = {
+                        Column(horizontalAlignment = CenterHorizontally) {
+                            if (item.badgeCount > 0) {
+                                BadgedBox(badge = {
+                                    Text(text = item.badgeCount.toString())
+                                }) {
+                                    Icon(
+                                        imageVector = item.icon,
+                                        contentDescription = item.name
+                                    )
+                                }
+                            } else {
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+                            }
+                            if (selected) {
+                                Text(
+                                    text = item.name,
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 10.sp
+                                )
+                            }
+
+                        }
+
+                    })
 
             }
+
+        }
+
+    }
+
+    @Composable
+    fun HomeScreen() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Text(text = "Home Screen")
+        }
+    }
+
+
+    @Composable
+    fun ChatScreen() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Text(text = "Home Screen")
+        }
+    }
+
+
+    @Composable
+    fun SettingScreen() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Text(text = "Home Screen")
         }
     }
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
-        MyApp {
-        }
+
     }
 
 
