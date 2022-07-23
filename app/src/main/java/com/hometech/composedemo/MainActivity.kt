2ea4d8f620271159b0a3fa2e174b845fcdaa3c8c
Arpit Patel <arpit@temi.ca>
2022-07-23 11:58:36 -0400

    Start bottom navigation from scratch. Base layout for three screen.

--- app/src/main/java/com/hometech/composedemo/MainActivity.kt
+++ app/src/main/java/com/hometech/composedemo/MainActivity.kt
@@ -29,169 +29,25 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.hometech.composedemo.ui.screens.HomeScreen
 import com.hometech.composedemo.ui.theme.ComposeDemoTheme
 
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeDemoTheme {
-                val navController = rememberNavController()
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigationBar(
-                            item = listOf(
-                                BottomNavigationItem(
-                                    name = "Home",
-                                    route = "home",
-                                    icon = Icons.Default.Home
-                                ),
-                                BottomNavigationItem(
-                                    name = "Chat",
-                                    route = "chat",
-                                    icon = Icons.Default.Notifications,
-                                    badgeCount = 23
-                                ),
-                                BottomNavigationItem(
-                                    name = "Settings",
-                                    route = "settings",
-                                    icon = Icons.Default.Settings,
-                                    badgeCount = 214
-                                ),
-                            ),
-                            navController = navController,
-                            onItemClick = {
-                                navController.navigate(it.route)
-                            }
-                        )
-                    }
-                ){
-                    Navigation(navController = navController)
-                }
-            }
+
+
         }
     }
 }
 
-    @Composable
-    fun Navigation(navController: NavHostController) {
 
-        NavHost(navController = navController, startDestination = "home") {
-
-            composable("home") {
-                HomeScreen()
-            }
-
-            composable("chat") {
-                ChatScreen()
-            }
-
-            composable("settings") {
-                SettingScreen()
-            }
-        }
-
-    }
-
-
-    @Composable
-    fun BottomNavigationBar(
-        item: List<BottomNavigationItem>,
-        navController: NavController,
-        modifier: Modifier = Modifier,
-        onItemClick: (BottomNavigationItem) -> Unit
-    ) {
-        val backStackEntry = navController.currentBackStackEntryAsState()
-        BottomNavigation(
-            modifier = modifier,
-            backgroundColor = Color.DarkGray,
-            elevation = 5.dp
-        ) {
-
-            item.forEach { item ->
-                val selected = item.route == backStackEntry.value?.destination?.route
-
-                BottomNavigationItem(
-                    selected = selected,
-                    onClick = { onItemClick(item) },
-                    selectedContentColor = Color.Green,
-                    unselectedContentColor = Color.DarkGray,
-                    icon = {
-                        Column(horizontalAlignment = CenterHorizontally) {
-                            if (item.badgeCount > 0) {
-                                BadgedBox(badge = {
-                                    Text(text = item.badgeCount.toString())
-                                }) {
-                                    Icon(
-                                        imageVector = item.icon,
-                                        contentDescription = item.name
-                                    )
-                                }
-                            } else {
-                                Icon(
-                                    imageVector = item.icon,
-                                    contentDescription = item.name
-                                )
-                            }
-                            if (selected) {
-                                Text(
-                                    text = item.name,
-                                    textAlign = TextAlign.Center,
-                                    fontSize = 10.sp
-                                )
-                            }
-
-                        }
-
-                    })
-
-            }
-
-        }
-
-    }
-
-    @Composable
-    fun HomeScreen() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-
-            Text(text = "Home Screen")
-        }
-    }
-
-
-    @Composable
-    fun ChatScreen() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-
-            Text(text = "Home Screen")
-        }
-    }
-
-
-    @Composable
-    fun SettingScreen() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-
-            Text(text = "Home Screen")
-        }
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-
-    }
-
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
 
 }
+
