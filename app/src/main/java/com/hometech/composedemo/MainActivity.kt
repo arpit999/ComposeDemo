e46611d20cbd96b32693f68e7c65a58bb9280099
arpit999 <arpitpatel009@gmail.com>
2023-02-25 13:32:00 -0500

    [search] Local search implemented with static list

--- app/src/main/java/com/hometech/composedemo/MainActivity.kt
+++ app/src/main/java/com/hometech/composedemo/MainActivity.kt
@@ -3,14 +3,23 @@ package com.hometech.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hometech.composedemo.domain.SearchViewModel
+import com.hometech.composedemo.model.Product
+import com.hometech.composedemo.model.allProducts
 import com.hometech.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +35,7 @@ class MainActivity : ComponentActivity() {
 
                 Surface(color = MaterialTheme.colors.background) {
 
+                    SearchView(searchText, products, isSearching, viewModel::onSearchText)
 
                 }
             }
@@ -33,18 +43,42 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun SearchField() {
+    fun SearchView(searchText: String, products: List<Product>, isSearching: Boolean, onSearchText: (String) -> Unit) {
 
-        Column {
+        Column(modifier = Modifier.fillMaxWidth()) {
 
+            OutlinedTextField(
+                value = searchText,
+                onValueChange = onSearchText,
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Search"
+                    )
+                })
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                items(products) { product ->
+                    Text(
+                        text = product.name,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp)
+                    )
+                }
+            }
         }
     }
 
-    //    @Preview(showBackground = true)
+    @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
         ComposeDemoTheme {
-
+            SearchView(searchText = "tooth", products = allProducts, isSearching = false, onSearchText = { })
         }
     }
 
